81d79f4ce7638c5adcc64b656658c92abd5dac64
Jamie Gardner <jagardner4@uclan.ac.uk>
2018-03-09 15:52:08 +0000

    Updated Sections

--- r2d2.js
+++ r2d2.js
@@ -1,13 +1,6 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
-<!-- R2D2 Head Section -->
-ctx.beginPath();
-ctx.moveTo(332,319);
-ctx.quadraticCurveTo(500,75,670,319);
-ctx.fillStyle = "#e7e7e7";
-ctx.fill();
-
 function drawRect(x,y,w,h,c) {
     ctx.fillStyle = c;
     ctx.fill();
@@ -20,6 +13,9 @@ drawRect(265,478,-11,200,'#82abdd'); <!-- R2D2 Leftarm inner-rectangle Section -
 drawRect(297,700,-21,21,'#e7e7e7'); <!-- R2D2 Leftarm inner-square Section -->
 drawRect(331,320,-11,160,'#e7e7e7'); <!-- R2D2 Leftarm to Head Connector -->
 
+drawRect(472,752,48,34,'#fff'); <!-- R2D2 Middlefoot Connector -->
+
+
 <!-- R2D2 Leftfoot Section -->
 ctx.beginPath();
 ctx.moveTo(320,743);
@@ -29,6 +25,7 @@ ctx.lineTo(215,826);
 ctx.lineTo(239,743);
 ctx.fillStyle = '#fff';
 ctx.fill();
+ctx.closePath();
 
 <!-- R2D2 Underbelly Section -->
 ctx.beginPath();
@@ -39,3 +36,24 @@ ctx.lineTo(634,711);
 ctx.lineTo(369,711);
 ctx.fillStyle = '#e7e7e7';
 ctx.fill();
+ctx.closePath();
+
+<!-- R2D2 Middlefoot Section -->
+ctx.beginPath();
+ctx.moveTo(472,785);
+ctx.lineTo(444,785);
+ctx.lineTo(420,869);
+ctx.lineTo(571,869);
+ctx.lineTo(547,785);
+ctx.fillStyle = '#fff';
+ctx.fill();
+ctx.closePath();
+
+<!-- R2D2 Head Section -->
+ctx.beginPath();
+ctx.moveTo(332,319);
+ctx.bezierCurveTo(340,100,660,100,670,319);
+ctx.fillStyle = '#e7e7e7';
+ctx.fill();
+
+drawRect(486,786,19,69,'#e7e7e7'); <!-- R2D2 Middlefoot inner-rectangle  -->
